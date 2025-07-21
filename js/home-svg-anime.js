1e47e2cf570fbfaa7127c53ae23b1daf592ff413
Yunpeng Wang <wang.yunpeng.cn@outlook.com>
2025-07-21 19:43:53 +0900

    Optimize the homepage SVG anime for mobile devices.

--- js/home-svg-anime.js
+++ js/home-svg-anime.js
@@ -1,31 +1,53 @@
-const socialIconSvg = document.querySelectorAll(".social-icons a svg")
-const svgLen = socialIconSvg.length
+// Modify the SVG animation on the homepage. 
+// This modification works only on mobile devices.
+// The animation for desktop is created using CSS.
+
+import { animate, svg, stagger } from "./anime.esm.min.js";
+
+const socialIconSvg = document.querySelectorAll(".social-icons a svg");
+const svgLen = socialIconSvg.length;
+const isProbablyTouch =
+  window.matchMedia("(pointer: coarse)").matches ||
+  "ontouchstart" in window ||
+  navigator.maxTouchPoints > 0;
 
 function createRandomInterval(min, max) {
-    // get intervel in [min, max)
-    return (Math.random() * (max -min)) + min
+  // get intervel in [min, max)
+  return Math.random() * (max - min) + min;
+}
+
+function addClass(arr, className) {
+  arr.forEach((element) => {
+    element.classList.add(className);
+  });
+}
+
+function deleteClass(arr, className) {
+  arr.forEach((element) => {
+    element.classList.remove(className);
+  });
 }
 
 function doRandomAnime() {
-    let count = Math.random() < 0.9 ? 1 : 2; // 90% 只动 1 个，30% 动 2 个
-    let selected = new Set();
+  let i = Math.floor(Math.random() * svgLen);
+  let selSvg = socialIconSvg[i];
+  let svgChild = selSvg.querySelectorAll("path, rect, polygon");
 
-    while (selected.size < count) {
-        let i = Math.floor(Math.random() * svgLen);
-        selected.add(i);
-    }
+  addClass(svgChild, "svg-anime-mobile");
 
-    for (let i of selected) {
-        let selSvg = socialIconSvg[i];
-        selSvg.classList.add("svg-anime-mobile");
+  animate(svg.createDrawable(".svg-anime-mobile"), {
+    draw: ["0 1", "1 1", "0 0", "0 1"],
+    ease: "inOutElastic(.5, .4)",
+    duration: 2000,
+    delay: stagger(100),
+    loop: false,
+  });
 
-        selSvg.addEventListener("animationend", function handler() {
-            selSvg.classList.remove("svg-anime-mobile");
-            selSvg.removeEventListener("animationend", handler);
-        });
-    }
-    setTimeout(doRandomAnime, createRandomInterval(1.5, 2.5) * 1000);
+  deleteClass(svgChild, "svg-anime-mobile");
+
+  setTimeout(doRandomAnime, createRandomInterval(5, 10) * 1000);
 }
 
-doRandomAnime();
-
+if (isProbablyTouch) {
+  doRandomAnime();
+}
